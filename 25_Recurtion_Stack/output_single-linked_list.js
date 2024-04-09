5afcfc3d2fabdc72d695752b3aba49df7193c566
Oleg <olegparamey4@gmail.com>
2024-04-09 16:06:44 +0200

    added alternative wawy of solving the task

--- 25_Recurtion_Stack/output_single-linked_list.js
+++ 25_Recurtion_Stack/output_single-linked_list.js
@@ -4,7 +4,17 @@ function printList(list){
         printList(list.next)
     }
 }
-
+/* Alternative way (Loop)
+function printList(list) {
+    let tmp = list;
+  
+    while (tmp) {
+      alert(tmp.value);
+      tmp = tmp.next;
+    }
+  
+  }
+*/
 
 let list = {
     value: 1,
