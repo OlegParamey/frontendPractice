e28e9fa96c1f34335d1ffa6dd63ed68106bae342
Oleg <olegparamey4@gmail.com>
2024-04-09 16:14:23 +0200

    added reversive way of solving

---
+++ 25_Recurtion_Stack/output_single-linked_list_inverted.js
@@ -0,0 +1,39 @@
+function printReverseList(list){
+    if (list.next){
+        printReverseList(list.next)
+    }
+    alert(list.value);
+}
+
+
+/* Alternative way (Loop)
+function printReverseList(list) {
+  let arr = [];
+  let tmp = list;
+
+  while (tmp) {
+    arr.push(tmp.value);
+    tmp = tmp.next;
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    alert( arr[i] );
+  }
+}
+*/
+
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+
+  printList(list);
