b1665344d044bca01fd2b0eea0dab3456db403ec
Oleg <olegparamey4@gmail.com>
2024-04-10 20:19:38 +0200

    added code - filter trough func

---
+++ 26_Variable_scope_Closure/filter_trough_func.js
@@ -0,0 +1,18 @@
+function inBetween(a, b){
+    return function(x){
+        return a <= x && x <= b;
+    }
+}
+
+function inArray(arr){
+    return function(x){
+        return arr.includes(x)
+    }
+}
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+alert( arr.filter(inArray([1, 2, 10, 4])) ); // 1,2
