eb165afc0d53bf7470e9700b83fe62352764a015
Oleg <olegparamey4@gmail.com>
2024-04-15 20:23:39 +0200

    added alternative way of solving the task

--- 28_Scheduling_setTimeout_and_setInterval/output_every_sec.js
+++ 28_Scheduling_setTimeout_and_setInterval/output_every_sec.js
@@ -1,4 +1,4 @@
-function printNumbers(from, to){
+function printNumbersTimeOut(from, to){
     let current = from;
     setTimeout(function go(){
         alert(current);
@@ -9,4 +9,17 @@ function printNumbers(from, to){
     }, 1000);
 }
 
-printNumbers(5, 10);
+function printNumbersInterval(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      alert(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+
+printNumbersTimeOut(5, 10);
+printNumbersInterval(5, 10);
