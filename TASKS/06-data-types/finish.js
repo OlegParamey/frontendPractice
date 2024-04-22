906dca94420d184e38871b68199da58f3721ae22
Oleg <olegparamey4@gmail.com>
2024-04-22 08:54:48 +0200

    one more simple task

--- TASKS/06-data-types/finish.js
+++ TASKS/06-data-types/finish.js
@@ -12,3 +12,19 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+let str = 'str';
+let num = 2;
+let bool = true;
+let none = null;
+let undef;
+let obj = { 1 : '12'};
+let array = [1, 2, 3, 4, 5];
+
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(bool));
+console.log(typeof(none));
+console.log(typeof(undef));
+console.log(typeof(obj));
+console.log(typeof(array));
+
