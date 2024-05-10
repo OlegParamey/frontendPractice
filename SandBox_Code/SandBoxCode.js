ab18b6737898e94aec7fd18e8b5e154675b0a1d6
Oleg <olegparamey4@gmail.com>
2024-05-10 11:22:26 +0200

    Practice with array methods

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,9 +1,7 @@
-function factorial(n) {
-    return (n != 1) ? n * factorial(n - 1) : 1;
-  }
+const myArray = [1, 2, 3, 4, 5];
+const newArray = myArray.map(function (el) {
+   return el * 3
+  });
 
-let my_Array= [1, 2, 3, 4, 5, 6];
-
-let factorialResults = my_Array.map(factorial);
-
-console.log(factorialResults); // Выводит: [1, 2, 6, 24, 120]
+console.log(myArray);
+console.log(newArray);
