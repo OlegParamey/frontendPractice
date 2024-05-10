9d87275d5fc4b81270338f86315e9ba76fb23a40
Oleg <olegparamey4@gmail.com>
2024-05-10 11:52:40 +0200

    some practice with Array Destructuring assignment, using lenght to get 2 last elements of an Array

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,6 +1,6 @@
 const myArray = ['apple', 'banana', 'orange'];
 const [one] = myArray;
-const [two, three] = myArray.slice(1); 
+const [two, three] = [myArray[myArray.length - 2], myArray[myArray.length - 1]]; 
 //console.log(one);
 console.log(two);
 console.log(three);
