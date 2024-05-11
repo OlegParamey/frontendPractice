4cd982a1c415e0308de8451525f1525f60e423c0
Oleg <olegparamey4@gmail.com>
2024-05-11 20:39:40 +0200

    practice with array method

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,14 +1,6 @@
-function Sum(a, b){
-  return console.log(a + b) 
-}
+const array = [1, 2, 3, 4, 5];
+array.length = 100;
+array.forEach((elem, index)=> console.log('element:',elem, 'index:', index));
+console.log(array.pop());
 
-function conslog(){
-  console.log('There is no numbers.')
-}
-
-const val1 = '10';
-const val2 = '5';
-
-typeof(val1) == 'number' && typeof(val2) == 'number'
-? Sum(+val1, +val2)
-: conslog()
+console.log(array.length);
