dcb05b13943f439b45ac206c96427739b7b4a953
Oleg <olegparamey4@gmail.com>
2024-05-11 20:32:26 +0200

    added some more practice code with ternary operator

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -6,9 +6,9 @@ function conslog(){
   console.log('There is no numbers.')
 }
 
-const val1 = 0;
-const val2 = 0;
+const val1 = '10';
+const val2 = '5';
 
 typeof(val1) == 'number' && typeof(val2) == 'number'
-? Sum(val1, val2)
+? Sum(+val1, +val2)
 : conslog()
