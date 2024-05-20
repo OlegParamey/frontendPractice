ff77a7f3c2c557448ea97b21f832ddaeec08b78d
Oleg <olegparamey4@gmail.com>
2024-05-20 09:10:21 +0200

    solved task using Array method .sort

--- TASKS/49-sort-array-of-objects/finish.js
+++ TASKS/49-sort-array-of-objects/finish.js
@@ -9,6 +9,7 @@
  * 3. Оригинальный массив должен остаться без изменений
  */
 
+
 const inputProducts = [
   {
     title: 'Phone case',
@@ -36,8 +37,12 @@ const inputProducts = [
   },
 ]
 
-// const sortedProducts = sortProductsByPrice(inputProducts)
+const sortProductsByPrice = (products) => 
+  [...products].sort((a ,b) => a.price - b.price)
 
-// console.log(sortedProducts) // Массив отсортированных товаров
 
-// console.log(inputProducts) // Оригинальный массив не должен измениться
+const sortedProducts = sortProductsByPrice(inputProducts)
+
+console.log(sortedProducts) // Массив отсортированных товаров
+
+console.log(inputProducts) // Оригинальный массив не должен измениться
