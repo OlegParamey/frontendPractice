305ecd6c07581e2c3d24b3703860153a45ebf136
Oleg <olegparamey4@gmail.com>
2024-05-21 17:47:21 +0200

    solved task using spread and rest operators, Array method .reduce

--- TASKS/51-rest-and-spread-operators/finish.js
+++ TASKS/51-rest-and-spread-operators/finish.js
@@ -13,21 +13,34 @@
  * так и оператор "spread".
  */
 
+const meanScore = (...args) =>{
+  if (args.some((element)=> typeof(element) !== 'number')){
+    console.error('Все аргументы в вызове функции должны быть числами!')
+    return
+  }
+
+
+  return args
+  .reduce((initialVal, value)=> initialVal + value/args.length, 0)
+  .toFixed(2)
+}
+
+
 const scores1 = [0, 1.5, 2.5, 3.7]
 const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
 const scores3 = [1.3, 2.5, 1.9]
 const scores4 = ['abc', 1.3, true, 2.5, 1.9]
 
-console.log(meanScore(/* все элементы из массива "scores1" */))
+console.log(meanScore(...scores1))
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+console.log(meanScore(...scores1, ...scores2))
 // 2.8
 
 console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
+  meanScore(...scores1, ...scores2, ...scores3)
 )
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */))
+console.log(meanScore(...scores4))
 // Все аргументы в вызове функции должны быть числами!
