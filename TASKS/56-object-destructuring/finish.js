c6bb36626b004a256f6af1115b7748d85dbbb88e
Oleg <olegparamey4@gmail.com>
2024-05-22 15:07:46 +0200

    solved task with Object destructuring

--- TASKS/56-object-destructuring/finish.js
+++ TASKS/56-object-destructuring/finish.js
@@ -5,8 +5,22 @@
  * 2. Объект, возвращаемый функцией "personInfo", должен содержать только сокращенные имена свойств
  */
 
-const personInfo = (/* parameters */) => {
-  /* return ... */
+const personInfo = (person) => {
+  const {
+    name,
+    age : personAge,
+    location:{country:origin, city:homeCity},
+    friendsQty = 0,
+    createdAtYear = new Date().getFullYear()
+  } = person
+  return {
+    name,
+    personAge,
+    origin,
+    homeCity,
+    friendsQty,
+    createdAtYear
+  }
 }
 
 const person = {
