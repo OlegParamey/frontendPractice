343ae2badc1e625cb650d1eb151cf31e2b4fa6e5
Oleg <olegparamey4@gmail.com>
2024-05-27 18:52:29 +0200

    solved task with creating operator-free function with passing values into it

--- TASKS/61-sum-numbers/finish.js
+++ TASKS/61-sum-numbers/finish.js
@@ -8,6 +8,21 @@
  * 3. Также верните эту сумму как результат функции
  */
 
+function sumNumbers2() {
+    let sum = 0;
+    for(let i = 0; i < arguments.length; i++){
+        sum += arguments[i]
+    }
+    console.log(sum)
+    return sum
+}
+
+const sumNumbers = (...numbers) =>{
+    const sum = numbers.reduce(((a, b) => a + b), 0)
+    console.log(sum)
+    return sum
+}
+
 sumNumbers(1, 3)
 // 4
 
@@ -15,4 +30,14 @@ sumNumbers(10, 20, 5)
 // 35
 
 sumNumbers(2, 5, 80, 1, 10, 12)
+// 110
+
+
+sumNumbers2(1, 3)
+// 4
+
+sumNumbers2(10, 20, 5)
+// 35
+
+sumNumbers2(2, 5, 80, 1, 10, 12)
 // 110
