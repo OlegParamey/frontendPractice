8c620ced81a0ced4662166753793bffbc6368168
Oleg <olegparamey4@gmail.com>
2024-05-27 19:05:20 +0200

    solved task with describing differences bewtween two function

--- TASKS/62-different-functions/finish.js
+++ TASKS/62-different-functions/finish.js
@@ -7,10 +7,26 @@
  * 3. Также вызовите обе функции
  */
 
+//we can invoke the function before it's declaration
+//console.log(firstFunction(2, 4))
+
 function firstFunction(a, b) {
   return a + b
 }
+//we can assign new value
+//firstFunction = 2;
+//console.log(firstFunction)
+
+//------------------------------------------------------------------------
+
+//We can't invoke the function before it's declaration
+//secondFunction(2, 6)
 
 const secondFunction = function (a, b) {
   return a + b
 }
+//We can't assing new value to the function
+//secondFunction = 2
+
+console.log(firstFunction(5, 5))
+console.log(secondFunction(1, 5))
