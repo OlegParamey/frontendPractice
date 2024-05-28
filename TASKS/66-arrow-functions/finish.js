ed51bce417b63b128fc78e78362b5dd3756f06a1
Oleg <olegparamey4@gmail.com>
2024-05-28 20:53:26 +0200

    solved task with Arrow function

--- TASKS/66-arrow-functions/finish.js
+++ TASKS/66-arrow-functions/finish.js
@@ -3,11 +3,9 @@
  * Замените обычные функции на стрелочные
  */
 
-function mult(a, b) {
-  return a * b
-}
+const mult = (a, b) => a * b
 
-setTimeout(function () {
+setTimeout(() => {
   console.log(mult(5, 10))
 }, 1000)
 // 50
