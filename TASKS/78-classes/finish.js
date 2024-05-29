27d758142a999b7b82e0100bb8142eb9606dd479
Oleg <olegparamey4@gmail.com>
2024-05-29 22:43:17 +0200

    solved task by creating new Class and adding in there custom method

--- TASKS/78-classes/finish.js
+++ TASKS/78-classes/finish.js
@@ -13,6 +13,18 @@
  * 4. Выводы в консоли должны совпасть
  */
 
+class Fruit{
+    constructor(name, price){
+        this.title = name,
+        this.price = price
+    }
+    
+    priceInfo(){
+        return `Price of the ${this.title} is ${this.price}$`
+    }
+
+}
+
 const apple = new Fruit('Apple', 2)
 console.log(apple.priceInfo())
 // Price of the Apple is 2$
