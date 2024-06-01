f34785e91a94a3627372e77c1c7970a3ccc03cd7
Oleg <olegparamey4@gmail.com>
2024-06-01 17:57:16 +0200

    solved task with function closure

--- TASKS/77-function-closures/finish.js
+++ TASKS/77-function-closures/finish.js
@@ -14,6 +14,19 @@
  *  - changeGreeting
  */
 
+function createGreeting(){
+    let greetingString = 'Hey, this is'
+
+    function greet(name){
+        return `${greetingString} ${name}`
+    }
+
+    function changeGreeting(newStr){
+        greetingString = newStr
+    }
+    return{greet, changeGreeting}
+}
+
 const greeting1 = createGreeting()
 
 console.log(greeting1.greet('Bob'))
