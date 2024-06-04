60de1b44785541e10ec23a4aad45094331f3fd2a
Oleg <olegparamey4@gmail.com>
2024-06-04 23:01:06 +0200

    solved task from CodeWars where you have to find the missing alphabet letter out of the given array of letters

---
+++ code_wars_code/6 kyu/Find_the_missing_letter.js
@@ -0,0 +1,19 @@
+function findMissingLetter(array)
+{
+  const alphabet = ['a', 'b', 'c', 'd', 'e'
+    , 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+    , 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'
+    , 'v', 'w', 'x', 'y', 'z'];
+  let j = 0;
+  for (let i = alphabet.indexOf(array[0].toLowerCase()); i <= alphabet.indexOf(array.at(-1).toLowerCase()); i++, j++ ){    
+    if(alphabet[i] !== array[j].toLowerCase()){
+        if(array[0] == array[0].toLowerCase()){
+            return alphabet[i];
+        }
+        return alphabet[i].toUpperCase();
+    }
+  } 
+}
+
+console.log(findMissingLetter(['a','b','c','d','f'])) // 'e'
+console.log(findMissingLetter(['O','Q','R','S'])) // 'P'
