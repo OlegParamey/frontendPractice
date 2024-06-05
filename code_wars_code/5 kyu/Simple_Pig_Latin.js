cf360522aa16229753a203a005b95ee7e43107a5
Oleg <olegparamey4@gmail.com>
2024-06-05 18:01:30 +0200

    solved kata from CodeWars Pig Latin

---
+++ code_wars_code/5 kyu/Simple_Pig_Latin.js
@@ -0,0 +1,16 @@
+function pigIt(str){
+    return str
+    .split(' ')
+    .map((element) => {
+        if(/[a-zA-Z]/.test(element)){
+        return element.slice(1) + element[0] + 'ay'
+    }else{
+        return element}
+  }).join(' ')
+}
+
+
+console.log(pigIt('Pig latin is cool')) // igPay atinlay siay oolcay
+console.log(pigIt('Hello world !'))     // elloHay orldway !
+
+
