983e683a64b76e4361968c1a7793e1873fdec81b
Oleg <olegparamey4@gmail.com>
2024-06-07 22:04:38 +0200

    added some practice code with Promises

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,34 +1,3 @@
-function solve(a, b) {
-    let counter = 0;
-
-    for (let i = a; i < b; i++) {
-        let str = i.toString();
-        let count8 = 0, count5 = 0, count3 = 0;
-        let isValid = true;
-
-        for (let char of str) {
-            if (char === '8') {
-                count8++;
-            } else if (char === '5') {
-                count5++;
-            } else if (char === '3') {
-                count3++;
-            } else {
-                isValid = false;
-                break;
-            }
-        }
-
-        if (isValid && count8 >= count5 && count5 >= count3) {
-            counter++;
-        }
-    }
-
-    return counter;
-}
-
-// Example usage
-console.log(solve(0, 1000)); // Expected output: 14
-
-
-console.log(solve(0, 100))
+const myPromise = new Promise((resolve, reject)=>{
+    
+});
