02cde128537c09dda80a4b3dc2002b2808c931ea
Oleg <olegparamey4@gmail.com>
2024-06-09 12:39:32 +0200

    Added some practice code with Promise, where new Promise has been creaated

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,4 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response=>response.json())
-    .then(json=>console.log(json))
-    .catch(error => console.error(error.message))
+const getData = (url) =>
+    new Promise((resolve, reject)=>
+        fetch(url)
+            .then(response=>response.json())
+            .then(json=>resolve(json))
+            .catch(error => reject(error))
+        )
+
+getData('https://jsonplaceholder.typicode.com/todos/55')
+    .then(data => console.log(data))
+    .catch(error => console.log(error.message))
