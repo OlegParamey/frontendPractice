991f219046b77fe145dfbd3286880560c6ca5b68
Oleg <olegparamey4@gmail.com>
2024-06-09 12:02:50 +0200

    some practice code with Promise

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,3 +1,4 @@
-const myPromise = new Promise((resolve, reject)=>{
-    
-});
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response=>response.json())
+    .then(json=>console.log(json))
+    .catch(error => console.error(error.message))
