af35cdab53b585ba29af87ce32428fa9bc641bf4
Oleg <olegparamey4@gmail.com>
2024-06-15 15:02:49 +0200

    added some practice code with async function and Promise

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,7 +1,13 @@
-async function myAsyncFn(){
-    throw new Error('There was an error!')
+const timerPromise = () =>
+    new Promise((resolve, reject)=>
+    setTimeout(() => resolve(), 2000))
+
+const asyncFn = async() =>{
+    console.log('Timer starts')
+    const startTime = performance.now()
+    await timerPromise()
+    const endTime = performance.now()
+    console.log('Timer ended', endTime - startTime)
 }
 
-myAsyncFn()
-    .then(value => console.log(value))
-    .catch(error => console.log(error.message))
+asyncFn()
