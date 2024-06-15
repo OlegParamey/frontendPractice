78e01dd6bce726e1032a9c643eaddf55307a9003
Oleg <olegparamey4@gmail.com>
2024-06-15 14:28:16 +0200

    created file with Promise example and added some practice code with async function

--- SandBox_Code/SandBoxCode.js
+++ SandBox_Code/SandBoxCode.js
@@ -1,11 +1,7 @@
-const getData = (url) =>
-    new Promise((resolve, reject)=>
-        fetch(url)
-            .then(response=>response.json())
-            .then(json=>resolve(json))
-            .catch(error => reject(error))
-        )
+async function myAsyncFn(){
+    throw new Error('There was an error!')
+}
 
-getData('https://jsonplaceholder.typicode.com/todos/55')
-    .then(data => console.log(data))
+myAsyncFn()
+    .then(value => console.log(value))
     .catch(error => console.log(error.message))
